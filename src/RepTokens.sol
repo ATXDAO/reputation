6aeb75c000a88cff9e561a34fa8b62162bf6d64d
Jacob Homanics <homanicsjake@gmail.com>
2023-08-26 00:49:08 -0500

    removed unnecesary function

--- src/RepTokens.sol
+++ src/RepTokens.sol
@@ -48,28 +48,6 @@ contract RepTokens is AccessControl, Ownable, ERC1155, Pausable {
         return string(abi.encodePacked(s_baseURI, Strings.toString(tokenId)));
     }
 
-    function uint2str(uint256 _i) internal pure returns (string memory _uintAsString) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 len;
-        while (j != 0) {
-            len++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(len);
-        uint256 k = len;
-        while (_i != 0) {
-            k = k - 1;
-            uint8 temp = (48 + uint8(_i - _i / 10 * 10));
-            bytes1 b1 = bytes1(temp);
-            bstr[k] = b1;
-            _i /= 10;
-        }
-        return string(bstr);
-    }
-
     function mint(address to, uint256 amount, bytes memory data) public onlyRole(MINTER_ROLE) whenNotPaused {
         require(amount <= s_maxMintAmountPerTx, "Cannot mint that many tokens in a single transaction!");
 
