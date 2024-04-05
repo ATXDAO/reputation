a82ae84a965f498c1008042ef6ef0eb3e37475fa
Jacob Homanics <homanicsjake@gmail.com>
2024-04-05 17:28:22 -0500

    removed inheritance from errors interface

--- contracts/IReputationTokensErrors.sol
+++ contracts/IReputationTokensErrors.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {IReputationTokensInternal} from
-    "./interfaces/IReputationTokensInternal.sol";
-
 /**
  * @title Interface for Reputation Tokens Internal
  * @author Jacob Homanics
@@ -11,7 +8,7 @@ import {IReputationTokensInternal} from
  * Hosts the error messages for Reputation Tokens Internal.
  * Additionally, inherits the proper events as well from Reputation Tokens Internal Interface.
  */
-interface IReputationTokensErrors is IReputationTokensInternal {
+interface IReputationTokensErrors {
     ///////////////////
     // Errors
     ///////////////////
