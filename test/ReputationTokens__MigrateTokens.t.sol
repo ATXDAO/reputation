dd3c66569eaadae20813956d3f34fef1608f7c90
Jacob Homanics <homanicsjake@gmail.com>
2024-04-05 15:01:06 -0500

    updated token migration to work with sequences

--- test/ReputationTokens__MigrateTokens.t.sol
+++ test/ReputationTokens__MigrateTokens.t.sol
@@ -1,78 +1,86 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import {Test, console} from "forge-std/Test.sol";
-// import {ReputationTokensStandalone} from "../contracts/ReputationTokensStandalone.sol";
-// import {IReputationTokensBaseInternal} from "../contracts/IReputationTokensBaseInternal.sol";
-// import {TokensPropertiesStorage} from "../contracts/storage/TokensPropertiesStorage.sol";
-// import {ReputationTokensInternal} from "../contracts/ReputationTokensInternal.sol";
-// import {ReputationTokensTest__Base} from "./ReputationTokensTest__Base.t.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {ReputationTokensStandalone} from "../contracts/ReputationTokensStandalone.sol";
+import {IReputationTokensBaseInternal} from "../contracts/IReputationTokensBaseInternal.sol";
+import {TokensPropertiesStorage} from "../contracts/storage/TokensPropertiesStorage.sol";
+import {ReputationTokensInternal} from "../contracts/ReputationTokensInternal.sol";
+import {ReputationTokensTest__Base} from "./ReputationTokensTest__Base.t.sol";
 
-// contract ReputationTokens__MigrateTokens is ReputationTokensTest__Base {
-//     ////////////////////////
-//     // Tests
-//     ////////////////////////
+contract ReputationTokens__MigrateTokens is ReputationTokensTest__Base {
+    ////////////////////////
+    // Tests
+    ////////////////////////
 
-//     function testMigrationOfTokens(
-//         TokensPropertiesStorage.TokenProperties[] memory tokensProperties,
-//         address user,
-//         address user2
-//     ) external {
-//         vm.assume(user != address(0));
-//         vm.assume(user2 != address(0));
+    function testMigrationOfTokens() external {
+        uint256 tokenId = createToken(
+            TokensPropertiesStorage.TokenProperties(
+                TokensPropertiesStorage.TokenType(0),
+                false,
+                false,
+                100
+            )
+        );
 
-//         batchCreateTokens(tokensProperties);
-//         ReputationTokensInternal.TokensOperations
-//             memory tokenOperations = createTokenOperationsSequential(
-//                 DISTRIBUTOR,
-//                 tokensProperties
-//             );
-//         mint(tokenOperations);
-//         ReputationTokensInternal.TokensOperations
-//             memory distributeOperations = createTokenOperationsSequential(
-//                 user,
-//                 tokensProperties
-//             );
-//         distribute(distributeOperations);
-//         vm.startPrank(user);
-//         s_repTokens.setApprovalForAll(TOKEN_MIGRATOR, true);
-//         vm.stopPrank();
-//         vm.startPrank(TOKEN_MIGRATOR);
-//         s_repTokens.migrateOwnershipOfTokens(user, user2);
-//         vm.stopPrank();
-//         for (uint256 i = 0; i < tokensProperties.length; i++) {
-//             assertEq(s_repTokens.balanceOf(user, i), 0);
-//             assertEq(
-//                 s_repTokens.balanceOf(user2, i),
-//                 tokensProperties[i].maxMintAmountPerTx
-//             );
-//         }
-//     }
+        address user1 = vm.addr(15);
+        address user2 = vm.addr(16);
 
-//     function testSetTokenURI(
-//         uint256 numOfTokens,
-//         string[] memory uris
-//     ) external {
-//         vm.assume(numOfTokens < uris.length);
-//         vm.startPrank(TOKEN_URI_SETTER);
-//         for (uint256 i = 0; i < numOfTokens; i++) {
-//             s_repTokens.setTokenURI(i, uris[i]);
-//         }
-//         vm.stopPrank();
-//         for (uint256 i = 0; i < numOfTokens; i++) {
-//             assertEq(s_repTokens.uri(i), uris[i]);
-//         }
-//     }
+        ReputationTokensInternal.Sequence memory mintSequence;
+        mintSequence.operations = new ReputationTokensInternal.Operation[](1);
+        mintSequence.to = DISTRIBUTOR;
 
-//     function testGetMaxMintPerTx(
-//         TokensPropertiesStorage.TokenProperties[] memory tokensProperties
-//     ) external {
-//         batchCreateTokens(tokensProperties);
-//         for (uint256 i = 0; i < tokensProperties.length; i++) {
-//             assertEq(
-//                 s_repTokens.getMaxMintPerTx(i),
-//                 tokensProperties[i].maxMintAmountPerTx
-//             );
-//         }
-//     }
-// }
+        mintSequence.operations[0].id = tokenId;
+        mintSequence.operations[0].amount = 100;
+
+        mint(mintSequence);
+
+        ReputationTokensInternal.Sequence memory distributeSequence;
+        distributeSequence
+            .operations = new ReputationTokensInternal.Operation[](1);
+        distributeSequence.to = user1;
+
+        distributeSequence.operations[0].id = tokenId;
+        distributeSequence.operations[0].amount = 100;
+
+        distribute(distributeSequence);
+
+        vm.startPrank(user1);
+        s_repTokens.setApprovalForAll(TOKEN_MIGRATOR, true);
+        vm.stopPrank();
+        vm.startPrank(TOKEN_MIGRATOR);
+        s_repTokens.migrateOwnershipOfTokens(user1, user2);
+        vm.stopPrank();
+
+        assertEq(s_repTokens.balanceOf(user1, 0), 0);
+        assertEq(s_repTokens.balanceOf(user2, 0), 100);
+    }
+
+    function testSetTokenURI(
+        uint256 numOfTokens,
+        string[] memory uris
+    ) external {
+        vm.assume(numOfTokens < uris.length);
+        vm.startPrank(TOKEN_URI_SETTER);
+        for (uint256 i = 0; i < numOfTokens; i++) {
+            s_repTokens.setTokenURI(i, uris[i]);
+        }
+        vm.stopPrank();
+        for (uint256 i = 0; i < numOfTokens; i++) {
+            assertEq(s_repTokens.uri(i), uris[i]);
+        }
+    }
+
+    function testGetMaxMintPerTx() external {
+        uint256 tokenId = createToken(
+            TokensPropertiesStorage.TokenProperties(
+                TokensPropertiesStorage.TokenType(0),
+                false,
+                false,
+                100
+            )
+        );
+
+        assertEq(s_repTokens.getMaxMintPerTx(tokenId), 100);
+    }
+}
