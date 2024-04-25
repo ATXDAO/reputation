bd88256745dc429429038969eb171df80958435e
Jacob Homanics <homanicsjake@gmail.com>
2024-04-25 02:19:46 -0500

    testing complete

--- test/ReputationTokensTest__Base.t.sol
+++ test/ReputationTokensTest__Base.t.sol
@@ -10,16 +10,15 @@ contract ReputationTokensTest__Base is Test {
     ////////////////////////
     // State Variables
     ////////////////////////
+
+    uint256 numOfTokenTypes = 3;
+
     address ADMIN = makeAddr("ADMIN");
     address TOKEN_CREATOR = makeAddr("TOKEN_CREATOR");
     address TOKEN_UPDATER = makeAddr("TOKEN_UPDATER");
-    address TOKEN_URI_SETTER = makeAddr("TOKEN_URI_SETTER");
     address MINTER = makeAddr("MINTER");
-    address BURNER = makeAddr("BURNER");
+    address TOKEN_URI_SETTER = makeAddr("TOKEN_URI_SETTER");
     address TOKEN_MIGRATOR = makeAddr("TOKEN_MIGRATOR");
-    address DESTINATION_WALLET = makeAddr("DESTINATION_WALLET");
-
-    address USER = makeAddr("USER");
 
     ReputationTokens s_repTokens;
 
@@ -49,33 +48,6 @@ contract ReputationTokensTest__Base is Test {
     ////////////////////////
     // Helper Functions
     ////////////////////////
-    // function batchCreateTokens(ReputationTokens.TokenType[] memory tokenType)
-    //     public
-    // {
-    //     vm.startPrank(TOKEN_CREATOR);
-    //     s_repTokens.batchCreateTokens(tokenType);
-    //     vm.stopPrank();
-    // }
-
-    // function createToken(ReputationTokens.TokenType tokenType)
-    //     public
-    //     returns (uint256)
-    // {
-    //     vm.startPrank(TOKEN_CREATOR);
-    //     uint256 tokenId = s_repTokens.createToken(tokenType);
-    //     vm.stopPrank();
-    //     return tokenId;
-    // }
-
-    // function createDefaultTokenWithAMintAmount() public returns (uint256) {
-    //     return createToken(ReputationTokens.TokenType.Transferable);
-    // }
-
-    // function createDefaultToken() public returns (uint256) {
-    //     return createToken(ReputationTokens.TokenType.Transferable);
-    // }
-
-    uint256 numOfTokenTypes = 3;
 
     function getNumTokenTypesMaxBound() public view returns (uint256) {
         return numOfTokenTypes - 1;
@@ -108,49 +80,6 @@ contract ReputationTokensTest__Base is Test {
                 115792089237316195423570985008687907852837564279074904382605163141518161494337
         );
         _;
-    }
-
-    modifier onlyValidAddresses(uint256[] memory ids) {
-        for (uint256 i = 0; i < ids.length; i++) {
-            vm.assume(ids[i] > 0);
-            vm.assume(
-                ids[i]
-                    <
-                    115792089237316195423570985008687907852837564279074904382605163141518161494337
-            );
-        }
-
-        _;
-    }
-
-    function batchUpdateTokens(
-        uint256[] memory ids,
-        ReputationTokens.TokenType[] memory _tokensType
-    ) public {
-        vm.startPrank(TOKEN_UPDATER);
-        s_repTokens.updateTokenBatch(ids, _tokensType);
-        vm.stopPrank();
-    }
-
-    function updateToken(
-        uint256 id,
-        ReputationTokens.TokenType tokenTypes
-    ) public {
-        vm.startPrank(TOKEN_UPDATER);
-        s_repTokens.updateToken(id, tokenTypes);
-        vm.stopPrank();
-    }
-
-    function mint(ReputationTokens.Sequence memory sequence) public {
-        vm.startPrank(MINTER);
-        // s_repTokens.mint(sequence);
-        vm.stopPrank();
-    }
-
-    function batchMint(ReputationTokens.Sequence[] memory sequences) public {
-        vm.startPrank(MINTER);
-        // s_repTokens.batchMint(sequences);
-        vm.stopPrank();
     }
 
     function setUpRole(bytes32 role, address addr) public {
