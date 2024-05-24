e2b843c86d51864f58c7edda1a172b30a19bfb6c
Jacob Homanics <homanicsjake@gmail.com>
2024-05-24 02:25:23 -0500

    Update ReputationTokensFactory.sol

--- contracts/ReputationTokensFactory.sol
+++ contracts/ReputationTokensFactory.sol
@@ -1,55 +1,55 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.13;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
 
-// import "@openzeppelin/contracts/access/AccessControl.sol";
-// import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
-// import {ReputationTokensUpgradeable} from "./ReputationTokensUpgradeable.sol";
+import {ReputationTokensUpgradeable} from "./ReputationTokensUpgradeable.sol";
 
-// contract ReputationTokensFactory is AccessControl {
-//     bytes32 public constant DEPLOYER_ROLE = keccak256("DEPLOYER_ROLE");
+contract ReputationTokensFactory is AccessControl {
+    bytes32 public constant DEPLOYER_ROLE = keccak256("DEPLOYER_ROLE");
 
-//     address public implementation;
+    address public implementation;
 
-//     uint256 public contractInstanceCount;
-//     mapping(uint256 => ReputationTokensUpgradeable) public instances;
+    uint256 public contractInstanceCount;
+    mapping(uint256 => ReputationTokensUpgradeable) public instances;
 
-//     event CreatedNewInstance(address);
+    event CreatedNewInstance(address);
 
-//     constructor(
-//         address[] memory _admins,
-//         address[] memory _controllers,
-//         address _implementation
-//     ) {
-//         for (uint256 i = 0; i < _admins.length; i++) {
-//             _grantRole(DEFAULT_ADMIN_ROLE, _admins[i]);
-//         }
+    constructor(
+        address[] memory _admins,
+        address[] memory _controllers,
+        address _implementation
+    ) {
+        for (uint256 i = 0; i < _admins.length; i++) {
+            _grantRole(DEFAULT_ADMIN_ROLE, _admins[i]);
+        }
 
-//         for (uint256 i = 0; i < _controllers.length; i++) {
-//             _grantRole(DEPLOYER_ROLE, _controllers[i]);
-//         }
+        for (uint256 i = 0; i < _controllers.length; i++) {
+            _grantRole(DEPLOYER_ROLE, _controllers[i]);
+        }
 
-//         if (_implementation != address(0)) {
-//             implementation = _implementation;
-//         }
-//     }
+        if (_implementation != address(0)) {
+            implementation = _implementation;
+        }
+    }
 
-//     function createNewInstance(
-//         address owner,
-//         address[] memory admins
-//     ) external onlyRole(DEPLOYER_ROLE) returns (address instanceAddress) {
-//         address clone = Clones.clone(address(implementation));
+    function createNewInstance(
+        address owner,
+        address[] memory admins
+    ) external onlyRole(DEPLOYER_ROLE) returns (address instanceAddress) {
+        address clone = Clones.clone(address(implementation));
 
-//         ReputationTokensUpgradeable instance =
-//             ReputationTokensUpgradeable(clone);
+        ReputationTokensUpgradeable instance =
+            ReputationTokensUpgradeable(clone);
 
-//         instance.initialize(owner, admins);
+        instance.initialize(owner, admins);
 
-//         instances[contractInstanceCount] = instance;
-//         contractInstanceCount++;
+        instances[contractInstanceCount] = instance;
+        contractInstanceCount++;
 
-//         emit CreatedNewInstance(address(instance));
+        emit CreatedNewInstance(address(instance));
 
-//         instanceAddress = address(instance);
-//     }
-// }
+        instanceAddress = address(instance);
+    }
+}
