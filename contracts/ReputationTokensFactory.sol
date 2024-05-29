e7d8895c7b9d0746598fd2bdadce3fab71821313
Jacob Homanics <homanicsjake@gmail.com>
2024-05-29 00:26:18 -0500

    factory distributes tokens

--- contracts/ReputationTokensFactory.sol
+++ contracts/ReputationTokensFactory.sol
@@ -4,10 +4,16 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import {ReputationTokensUpgradeable} from "./ReputationTokensUpgradeable.sol";
 
 contract ReputationTokensFactory is AccessControl {
     bytes32 public constant DEPLOYER_ROLE = keccak256("DEPLOYER_ROLE");
+
+    uint256 constant OP_AMOUNT = 1 ether;
+
+    IERC20 optimisTokens;
 
     address public s_implementation;
 
@@ -16,7 +22,7 @@ contract ReputationTokensFactory is AccessControl {
 
     event CreatedNewInstance(address indexed creator, address indexed instance);
 
-    constructor(address[] memory _admins, address implementation) {
+    constructor(address[] memory _admins, address implementation, address op) {
         for (uint256 i = 0; i < _admins.length; i++) {
             _grantRole(DEFAULT_ADMIN_ROLE, _admins[i]);
         }
@@ -24,6 +30,8 @@ contract ReputationTokensFactory is AccessControl {
         if (implementation != address(0)) {
             s_implementation = implementation;
         }
+
+        optimisTokens = IERC20(op);
     }
 
     function createNewInstance(
@@ -45,6 +53,9 @@ contract ReputationTokensFactory is AccessControl {
         emit CreatedNewInstance(msg.sender, address(instance));
 
         instanceAddress = address(instance);
+
+        if (address(optimisTokens) != address(0))
+            optimisTokens.transferFrom(address(this), msg.sender, OP_AMOUNT);
     }
 
     function setImplementation(address implementation) external {
